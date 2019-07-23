8a81a3b18f840566f19a30736a5029309d416589
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-07-23 22:25:43 +0200

    Added jquery

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,7 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-  const checkboxes = document.getElementsByTagName('input');
-  for (var i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener('click', clickHandler);
-  }
+$(function() {
+  $('input').on('click', function() {
+    $(this)
+      .parent()
+      .toggleClass('checked');
+  });
 });
-
-function clickHandler() {
-  this.checked
-    ? (this.parentNode.className = 'checked')
-    : (this.parentNode.className = '');
-}
