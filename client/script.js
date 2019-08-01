87dfc272075332237427372dc3f3623b9683a29c
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-08-01 21:55:57 +0200

    Added delete api call

--- client/script.js
+++ client/script.js
@@ -22,6 +22,36 @@ $(function() {
     });
   };
 
+  const updateTodo = function(id, data, cb) {
+    $.ajax({
+      url: '/api/todos/' + id,
+      type: 'PUT',
+      data: data,
+      dataType: 'json',
+      success: function(data) {
+        cb();
+      }
+    });
+  };
+
+  const deleteTodo = function(id, cb) {
+    $.ajax({
+      url: '/api/todos/' + id,
+      type: 'DELETE',
+      data: {
+        _id: id
+      },
+      dataType: 'json',
+      success: function(data) {
+        cb();
+      }
+    });
+  };
+
+  const deleteTodoLi = function($li) {
+    $li.remove();
+  };
+
   $(':button').on('click', addTodo);
 
   $(':text').on('keypress', function(e) {
@@ -34,9 +64,51 @@ $(function() {
     }
   });
 
-  $('input').on('click', function() {
-    $(this)
-      .parent()
-      .toggleClass('checked');
+  $('ul').on('change', 'li :checkbox', function() {
+    const $this = $(this),
+      $input = $this[0],
+      $li = $this.parent(),
+      id = $li.attr('id'),
+      checked = $input.checked,
+      data = { done: checked };
+    updateTodo(id, data, function(d) {
+      $this.next().toggleClass('checked');
+    });
+  });
+
+  $('ul').on('keydown', 'li span', function(e) {
+    const $this = $(this),
+      $span = $this[0],
+      $li = $this.parent(),
+      id = $li.attr('id'),
+      key = e.keyCode,
+      target = e.target,
+      text = $span.innerHTML,
+      data = { text: text };
+    $this.addClass('editing');
+    if (key === 27) {
+      //escape key
+      $this.removeClass('editing');
+      document.execCommand('undo');
+      target.blur();
+    } else if (key === 13) {
+      //enter key
+      updateTodo(id, data, function(d) {
+        $this.removeClass('editing');
+        target.blur();
+      });
+      e.preventDefault();
+    }
+  });
+
+  $('ul').on('click', 'li a', function() {
+    const $this = $(this),
+      $input = $this[0],
+      $li = $this.parent(),
+      id = $li.attr('id');
+
+    deleteTodo(id, function(e) {
+      deleteTodoLi($li);
+    });
   });
 });
--- routes/todos/index.js
+++ routes/todos/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Todo = require('../../models/todos');
 
 router.get('/', function(req, res, next) {
-  Todo.findAsync()
+  Todo.findAsync({}, null, { sort: { _id: 1 } })
     .then(function(todos) {
       res.render('todos', { title: 'Todos', todos: todos });
     })
